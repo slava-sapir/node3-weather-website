bd9f852a87d8d748908218e18ffbd26477ba9ada
slava sapir <slavas21@gmail.com>
2020-02-13 23:01:42 -0500

    some changes

--- src/app.js
+++ src/app.js
@@ -87,7 +87,7 @@ app.get('/products', (req, res) => {
 app.get('/help/*', (req, res) => {
     res.render('404', {
         title: '404',
-        name: 'Andrew Mead',
+        name: 'Slava Sapir',
         errorMessage: 'Help article not found.'
     })
 })
@@ -95,7 +95,7 @@ app.get('/help/*', (req, res) => {
 app.get('*', (req, res) => {
     res.render('404', {
         title: '404',
-        name: 'Andrew Mead',
+        name: 'Slava Sapir',
         errorMessage: 'Page not found.'
     })
 })
